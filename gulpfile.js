d2c0eb4e095c8c34bfcde7cabd5b67eda77a1c7e
Targonkp Targonkp <targon_k_p@mail.ru>
2022-06-18 14:21:41 +0300

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const {src, dest, watch, series, parallel} = require("gulp");
+const prettier = require("gulp-prettier");
+const autoprefixer = require("gulp-autoprefixer");
+const csso = require("gulp-csso");
+const htmlmin = require("gulp-htmlmin");
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+
+const formatter = () => {
+    return src("src/**/*.{html,css,js}")
+        .pipe(prettier({ singleQuote: true }))
+        .pipe(dest("./dist"))
+        .pipe(browserSync.stream())
+}
+
+const html = () => {
+    return src("./src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest("./dist"))
+}
+
+const cssPrefix = () => {
+    return src("./dist/styles/*.css")
+        .pipe(autoprefixer())
+        .pipe(dest("./dist/styles"))
+}
+
+const css = () => {
+    return src("./dist/styles/*.css")
+        .pipe(csso())
+        .pipe(dest("./dist/mini-files"))
+}
+
+const js = () => {
+    return src("./dist/js/*.js")
+        .pipe(terser())
+        .pipe(dest("./dist/mini-files"))
+}
+
+const img = () => {
+    return src("./src/images/*{png,jpg,jpeg,gif,svg}")
+        .pipe(imagemin(
+            { verbose: true}
+        ))
+        .pipe(dest("./dist/images"))
+}
+
+const server = () => {
+    browserSync.init(
+        {
+            server: {
+                baseDir: "./dist"
+            }
+        }
+    )
+}
+
+const clear = () => {
+    return del("./dist");
+}
+
+const watcher = () => {watch("./src/**/*.{html,css,js}", series(formatter, cssPrefix, css, html, js, img))}
+
+exports.dev = series(
+    clear,
+    formatter,
+    cssPrefix,
+    css,
+    js,
+    html,
+    img,
+    parallel(watcher, server)
+);
--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,37 @@ document.querySelector('.burger-menu').addEventListener(
     () => {
         menu.classList.toggle('burger-menu-active');
     }
+);
+
+document.querySelector('.interior-menu').addEventListener(
+    'click',
+    (event) => {
+        if (event.target.classList.contains('interior-menu__text'))
+        {
+            document.querySelectorAll('.interior-menu__text').forEach(
+                item => item.classList.remove('interior-menu__text_active')
+            )
+            event.target.classList.add('interior-menu__text_active');
+        }
+    }
 )
+
+document.querySelector('.header-menu').addEventListener(
+    'click',
+    (event) => {
+        if (event.target.classList.contains('header-menu__link')){
+            document.querySelectorAll('.header-menu__link').forEach(
+                item => item.classList.remove('header-menu__link_active')
+            )
+            event.target.classList.add('header-menu__link_active');
+        }
+    }
+)
+
+function animation (){
+    let companyNameEl = document.querySelector('.header-company-name');
+    companyNameEl.style.animationDuration = '2s';
+}
+
+animation ();
+
